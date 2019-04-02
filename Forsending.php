7e35ab867321ad13338d8d3dce42808c8cc6c7eb
sprichan <49210623+sprichan@users.noreply.github.com>
2019-04-02 15:53:41 -0400

    Initial commit

---
+++ Forsending.php
@@ -0,0 +1,59 @@
+<?php
+
+// NOTE: Be sure to uncomment the following line in your php.ini file.
+// ;extension=php_openssl.dll
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the accessKey string value with your valid access key.
+$accessKey = '6b2dffb76b0c484b87925f3a89372dc9';
+
+// Replace or verify the region.
+
+// You must use the same region in your REST API call as you used to obtain your access keys.
+// For example, if you obtained your access keys from the westus region, replace 
+// "westcentralus" in the URI below with "westus".
+
+// NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+// a free trial access key, you should not need to change this region.
+$host = 'https://eastus.api.cognitive.microsoft.com';
+$path = '/text/analytics/v2.0/keyPhrases';
+
+function GetKeyPhrases ($host, $path, $key, $data) {
+
+	$headers = "Content-type: text/json\r\n" .
+		"Ocp-Apim-Subscription-Key: $key\r\n";
+
+	$data = json_encode ($data);
+
+	// NOTE: Use the key 'http' even if you are making an HTTPS request. See:
+	// https://php.net/manual/en/function.stream-context-create.php
+	$options = array (
+		'http' => array (
+			'header' => $headers,
+			'method' => 'POST',
+			'content' => $data
+		)
+	);
+	$context  = stream_context_create ($options);
+	$result = file_get_contents ($host . $path, false, $context);
+	return $result;
+}
+
+$data = array (
+	'documents' => array (
+		array ( 'id' => '1', 'language' => 'en', 'text' => 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' ),
+		array ( 'id' => '2', 'language' => 'es', 'text' => 'Si usted quiere comunicarse con Carlos, usted debe de llamarlo a su telefono movil. Carlos es muy responsable, pero necesita recibir una notificacion si hay algun problema.' ),
+		array ( 'id' => '3', 'language' => 'en', 'text' => 'The Grand Hotel is a new hotel in the center of Seattle. It earned 5 stars in my review, and has the classiest decor I\'ve ever seen.' )
+	)
+);
+
+print "Please wait a moment for the results to appear.";
+
+$result = GetKeyPhrases ($host, $path, $accessKey, $data);
+
+echo json_encode (json_decode ($result), JSON_PRETTY_PRINT);
+?>
+
